7663b69211450a555c81ba17e6ec771cda3fe735
vusallyv <abdulla.vusal.3@gmail.com>
2025-05-26 14:14:58 +0300

    Task 3: Add Kafka listener to TeacherService for consuming appointment events and saving them to the database

--- teacherservice/src/main/java/com/esi/teacherservice/service/TeacherService.java
+++ teacherservice/src/main/java/com/esi/teacherservice/service/TeacherService.java
@@ -1,7 +1,10 @@
 package com.esi.teacherservice.service;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
+import com.esi.teacherservice.dto.AppointmentDto;
+import com.esi.teacherservice.model.Appointment;
+import com.esi.teacherservice.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,4 +13,21 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class TeacherService {
 
+    private final TeacherRepository teacherRepository;
+
+    @KafkaListener(topics = "appointmentTopic", groupId = "teacher-service-group")
+    public void consumeAppointment(AppointmentDto appointmentDto) {
+        log.info("Received appointment event: {}", appointmentDto);
+
+        Appointment appointment = Appointment.builder()
+                .appointmentid(appointmentDto.getAppointmentid())
+                .studentId(appointmentDto.getStudentId())
+                .studentName(appointmentDto.getStudentName())
+                .teacherId(appointmentDto.getTeacherId())
+                .date(appointmentDto.getDate())
+                .build();
+
+        teacherRepository.save(appointment);
+        log.info("Appointment saved to teacher service database: {}", appointment);
+    }
 }
