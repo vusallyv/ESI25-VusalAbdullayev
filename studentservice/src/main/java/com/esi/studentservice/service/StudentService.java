fd73d1c75da6df33359d8ef917138a926f03720a
vusallyv <abdulla.vusal.3@gmail.com>
2025-05-26 14:12:33 +0300

    Task 1: Refactor StudentService to include StudentRepository and save student data in addStudentInfo method

--- studentservice/src/main/java/com/esi/studentservice/service/StudentService.java
+++ studentservice/src/main/java/com/esi/studentservice/service/StudentService.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Service;
 import com.esi.studentservice.dto.StudentDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import com.esi.studentservice.model.Student;
+import com.esi.studentservice.repository.StudentRepository;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class StudentService {
 
-    public void addStudentInfo(StudentDto studentDto) {
+    private final StudentRepository studentRepository;
 
+    public void addStudentInfo(StudentDto studentDto) {
+        Student student = Student.builder()
+                .studentId(studentDto.getStudentId())
+                .studentName(studentDto.getStudentName())
+                .studentData(studentDto.getStudentData())
+                .build();
+        studentRepository.save(student);
     }
 
 }
